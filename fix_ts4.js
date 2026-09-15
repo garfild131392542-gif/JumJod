a7494a1c0ec91158c0bba60411029530a285cb07
Garfield <garfild.13139.2542@gmail.com>
2026-09-15 14:34:03 +0700

    chore: remove temporary fix and utility scripts

--- fix_ts4.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/lib/line/flex-templates.ts', 'utf8');
-content = content.replace(
-  "contents: [ { type: 'text', text: `และอีก ${items.length - 11} รายการ...`, weight: 'bold', wrap: true } ]",
-  "contents: [ { type: 'text', text: `และอีก ${items.length - 11} รายการ...`, weight: 'bold', size: 'sm', wrap: true, color: '#64748b' } ]"
-);
-fs.writeFileSync('src/lib/line/flex-templates.ts', content);
--- fix_ts5.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/lib/line/flex-templates.ts', 'utf8');
-content = content.replace(
-  "const bubbles = items.slice(0, 11).map(item => {",
-  "const bubbles: any[] = items.slice(0, 11).map(item => {"
-);
-fs.writeFileSync('src/lib/line/flex-templates.ts', content);
