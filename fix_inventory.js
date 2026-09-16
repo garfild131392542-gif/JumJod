fd09105f401bf4fb389ce77bc97ab5fae359019f
Garfild <Sakditach25@gmail.com>
2026-09-16 09:14:30 +0700

    chore: remove obsolete fix and patch scripts from root directory

--- fix_inventory.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const targetStr = "if (cmd.action === 'ADD' || cmd.action === 'UPDATE' || cmd.action === 'ADD_STOCK' || cmd.action === 'SUBTRACT_STOCK' || cmd.action === 'CHECK_STOCK') {";
-const replacementStr = "if (['ADD', 'UPDATE', 'ADD_STOCK', 'SUBTRACT_STOCK', 'CHECK_STOCK', 'DELETE'].includes(cmd.action)) {";
-let code = fs.readFileSync('src/lib/line/handlers/central-router.ts', 'utf8');
-code = code.replace(targetStr, replacementStr);
-fs.writeFileSync('src/lib/line/handlers/central-router.ts', code);
-console.log('Fixed INVENTORY block');
